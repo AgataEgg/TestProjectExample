5ff15df7ae3dcd9a512affdf06b30dbaba93b611
agataegg@outlook.com <agataegg@outlook.com>
2022-05-19 22:17:19 +0700

    update

---
+++ Турфирма_Притужалова_Д199/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Турфирма_Притужалова_Д199
+{
+    /// <summary>
+    /// Логика взаимодействия для App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
---
+++ Турфирма_Притужалова_Д199/Manager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Турфирма_Притужалова_Д199
+{
+    class Manager
+    {
+        public static Frame MainFrame
+        {
+            get;
+            set;
+        }
+    }
+}
